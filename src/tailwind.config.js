87e14c206d046fc525b9094f3152c9c1366e7cdf
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-04-13 15:10:45 +0600

    st step done. without responsive and hover effect

---
+++ src/tailwind.config.js
@@ -0,0 +1,36 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//         extend: {
+//             fontFamily: {
+//                 poppins: ["Poppins", "sans-serif"],
+//             },
+//         },
+//     },
+//     plugins: [],
+// }
+
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'btnColor': '#F9AC54',
+                'tahiti': '#3ab7bf',
+                'bermuda': '#78dcca',
+            },
+        },
+    },
+}
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      textColor: {
+        custom: "rgb(209, 213, 219)",
+      },
+    },
+  },
+};
